06ac1749b7452cae9f6b2725efeed721a4d2283d
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-06-09 03:40:30 +0600

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="h-screen banner-extra-style relative">
+        <h1 className="text-xl text-white absolute top-[900px] left-[45%]">DOWN</h1>
+      </div>
+      ksadjflksjdflkj
+    </div>
+  );
+};
+
+export default Home;
