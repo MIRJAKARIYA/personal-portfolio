a90901ff9c8b5de252f72e481074fdb21e959373
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-06-10 16:35:05 +0600

    skills added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ContactMe from './components/ContactMe/ContactMe';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Projects from './components/Projects/Projects';
+import Skills from './components/Skills/Skills';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar></Navbar>
       <Home></Home>
       <About></About>
+      <Skills></Skills>
       <Projects></Projects>
       <ContactMe></ContactMe>
     </div>
